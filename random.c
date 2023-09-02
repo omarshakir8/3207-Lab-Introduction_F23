65cc790ea2540e550aeb820597b341bfeaf0b2d9
omarshakir8 <omarshakir8@gmail.com>
2023-09-02 01:10:30 -0400

    Rand Char Function Implemented

---
+++ random.c
@@ -0,0 +1,12 @@
+//
+// Created by Omar Shakir on 9/2/23.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "random.h"
+
+char randchar() {
+    return 'A' + rand() % 26; //
+}
+
---
+++ random.h
@@ -0,0 +1,8 @@
+//
+// Created by Omar Shakir on 9/2/23.
+//
+
+#ifndef INC_3207_LAB_INTRODUCTION_F23_RANDOM_H
+#define INC_3207_LAB_INTRODUCTION_F23_RANDOM_H
+char randchar();
+#endif //INC_3207_LAB_INTRODUCTION_F23_RANDOM_H
